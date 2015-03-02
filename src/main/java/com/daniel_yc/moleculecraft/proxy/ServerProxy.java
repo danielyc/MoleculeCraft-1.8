334ea5388e302cb48b2ad3308618732828ea8bb1
danielyc <danielyc.moddev@gmail.com>
2015-03-02 15:57:43 +0100

    initial commit
    initial commit to github

---
+++ src/main/java/com/daniel_yc/moleculecraft/proxy/ServerProxy.java
@@ -0,0 +1,20 @@
+package com.daniel_yc.moleculecraft.proxy;
+
+public class ServerProxy {
+	
+	public void registerRenderThings(){
+		
+		
+	}
+	
+	public void registerNetworkStuff(){
+		
+		
+	}
+	
+	public void registerTileEntities(){
+		
+		
+	}
+
+}
